bbd8e732c3fcd81b1e333a561bb8380dea02e845
Challa-pranavchandan <challapranavchandan@gmail.com>
2026-02-03 11:16:27 +0530

    resatrt

---
+++ react_day1/tailwind+react/tailwind/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Card from './components/card'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className="rounded-lg bg-red-600">Hello Tailwind</h1>
+   <Card/>
+    </> 
+  )
+}
+
+export default App
